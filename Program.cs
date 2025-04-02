8e95397e77634d46e686b40c9f70e83c70ecb50a
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-04-02 12:35:15 -0600

    Intro

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Chandler Wixom, 4/2/2025, lab 9 maze part 2
+
+// Introduction
+Console.BackgroundColor = ConsoleColor.DarkGray;
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.Clear();
+
+Console.WriteLine(" Hello, This is a maze game. Your job is to reach the (#) while avoiding the (%)\n\n Use \tw, a, s, d   or   Up, Down, Left, Right Arrow keys to move.\n\n Press Any key to start \n\tEsc to exit");
